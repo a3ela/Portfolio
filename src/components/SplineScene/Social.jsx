a903ce15a3462bef21876d1d5bcd2b483a5cb9a1
abel <abelsintayehu626@gmail.com>
2024-12-11 14:59:14 +0300

    change: change in icon postion

--- src/components/SplineScene/Social.jsx
+++ src/components/SplineScene/Social.jsx
@@ -3,10 +3,10 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 const Social = () => {
   return (
     <div className="hero__social">
-      <a href="" className="hero__social-icon" target="_blank">
+      <a href="#" className="hero__social-icon" target="_blank">
         <FaGithub />
       </a>
-      <a href="" className="hero__social-icon" target="_blank">
+      <a href="#" className="hero__social-icon" target="_blank">
         <FaLinkedin />
       </a>
     </div>
