a7c70299cc713bcbd28f9daeb08f0d83c5993f05
abel <abelsintayehu626@gmail.com>
2024-12-12 00:26:34 +0300

    add: component added for contact

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section className="contact section">
+      <div className="contact__content">
+        <img src="" alt="" />
+        <div className="contact__data">
+          <h2 className="contact__description">Let's have a chat!</h2>
+          <a href="mailto:webwizabel@gmail.com" className="button button--flex">
+            Contact me
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
