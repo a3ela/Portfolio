bb18522c2a9e60a1fcff37cedf7fc6d62f3495c9
abel <abelsintayehu626@gmail.com>
2024-12-11 15:00:50 +0300

    remove: removed padding for the seciotn

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,43 +8,43 @@ import {
   IoGitBranch,
 } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
-
+import logo from "../../assets/logo.png";
 const Header = () => {
   const [toggle, setToggle] = useState(false);
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="index.html" className="nav__logo">
-          Abela
+        <a href="#" className="nav__logo">
+          <img src={logo} alt="logo" />
         </a>
         <div className={toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a href="#home" className="nav__link active-link">
+              <a href="#" className="nav__link active-link">
                 <IoHomeOutline className="nav__icon" />
                 Home
               </a>
             </li>
             <li className="nav__item">
-              <a href="#about" className="nav__link">
+              <a href="#" className="nav__link">
                 <IoHomeOutline className="nav__icon" />
                 About
               </a>
             </li>
             <li className="nav__item">
-              <a href="#projects" className="nav__link">
+              <a href="#" className="nav__link">
                 <IoClipboardOutline className="nav__icon" />
                 Projects
               </a>
             </li>
             <li className="nav__item">
-              <a href="#skills" className="nav__link">
+              <a href="#" className="nav__link">
                 <IoGitBranch className="nav__icon" />
                 SKills
               </a>
             </li>
             <li className="nav__item">
-              <a href="#contact" className="nav__link">
+              <a href="#" className="nav__link">
                 <IoChatbubbleEllipsesOutline className="nav__icon" />
                 Contact
               </a>
