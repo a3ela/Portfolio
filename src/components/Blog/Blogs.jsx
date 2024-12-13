db6e43f8c3d4e8797d1690e7be8e4975af5ec477
abel <abelsintayehu626@gmail.com>
2024-12-13 15:15:46 +0300

    feat: blog feature added with medium api

--- src/components/Blog/Blogs.jsx
+++ src/components/Blog/Blogs.jsx
@@ -2,6 +2,21 @@ import React from "react";
 import { useEffect, useState } from "react";
 import blogService from "../../services/medium";
 import "./blog.css";
+
+const getContentOfBlog = (blog) => {
+  const startingPoint = blog.indexOf("<p>");
+  const relevantText = blog.slice(startingPoint + 3);
+
+  return relevantText.substr(0, 100) + "...";
+};
+
+function extractImageUrl(html) {
+  const tempDiv = document.createElement("div");
+  tempDiv.innerHTML = html;
+  const imgElement = tempDiv.querySelector("img");
+  return imgElement ? imgElement.src : null;
+}
+
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
   useEffect(() => {
@@ -10,30 +25,37 @@ const Blogs = () => {
     });
   }, []);
 
-  const getContentOfBlog = (blog) => {
-    const startingPoint = blog.indexOf("<p>");
-    const relevantText = blog.slice(startingPoint + 3);
-
-    return relevantText.substr(0, 100) + "...";
-  };
-
   return (
     <section className="blog section" id="section">
+      <div className="section__head container">
+        <h1 className="section__title">Blog</h1>
+        <h2 className="section__subtitle">some of my posts</h2>
+      </div>
       <div className="blogs__container container grid">
-        {blogs.map((blog) => (
-          <article className="blogs__content">
-            <div className="blogs__head">
-              <h2 className="blogs__title">{blog.title}</h2>
-              <p>{blog.pubDate.split(" ")[0]}</p>
-            </div>
-            <p className="blogs__descripition">
-              {getContentOfBlog(blog.content)}
-            </p>
-            <a href={blog.link} target="_blank">
-              Read More
-            </a>
-          </article>
-        ))}
+        {blogs.map((blog) => {
+          const backgroundImageUrl = blog.content
+            ? extractImageUrl(blog.content)
+            : "";
+          return (
+            <article
+              className="blogs__content"
+              style={{
+                backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, .8), rgba(0, 0, 0, .8)), url(${backgroundImageUrl})`,
+              }}
+            >
+              <div className="blogs__head">
+                <h2 className="blogs__title">{blog.title}</h2>
+                <p>{blog.pubDate.split(" ")[0]}</p>
+              </div>
+              <p className="blogs__description">
+                {getContentOfBlog(blog.content)}
+              </p>
+              <a href={blog.link} target="_blank" rel="noopener noreferrer">
+                Read More
+              </a>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
